8d62246bfc210fd57d7bcf9248857b1cd38354a8
yeniferjpc <157909782+yeniferjpc@users.noreply.github.com>
2024-04-13 13:03:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let femenino;
+let calculo;
+
+let peso = prompt("Ingrese su peso en kilogramos")
+let sexo = prompt("Ingrese sexo: femenino o masculino")
+
+if(sexo==="femenino") 
+{
+calculo = peso*2;
+console.log("calculo")
+}
+
+else 
+{
+    calculo = peso*1,8;
+    console.log("calculo")
+}
